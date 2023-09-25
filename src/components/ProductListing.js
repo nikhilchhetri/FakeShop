9ef049c3fc1ff64bc9b689d3635e68783913d329
nikhilchhetri <nikhilchhetri09@gmail.com>
2023-09-25 12:21:50 +0530

    final commit

---
+++ src/components/ProductListing.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
+import axios from "axios";
+import { setProducts } from "../Redux/actions/productActions";
+const ProductListing = () => {
+  const dispatch = useDispatch();
+  const fetchData = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Error", err);
+      });
+    dispatch(setProducts(response.data));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
+
+export default ProductListing;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import ProductDetail from "./components/ProductDetail";
+import ProductListing from "./components/ProductListing";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
-import "semantic-ui-css/semantic.min.css";
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <ProductListing />,
+      },
+      {
+        path: "/product/:productId",
+        element: <ProductDetail />,
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={appRouter} />
+  </Provider>
 );
